d459847bf0fd05a7d76e89d88e87ad1a3fa6a8bc
Sam Baskar <baskars@gmail.com>
2016-03-22 14:19:50 -0700

    my first c
    my first c program

---
+++ myFirst.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void swapMe(int *x, int * y)
+{
+   int temp;
+   temp = (int) *x;
+   *x = *y;
+   *y = temp;
+   
+
+}
+
+
+
+
+void main()
+{
+   int a = 5;
+   int b = 10;
+
+   printf ("Hi\n");
+   printf("Before Change: `a is %d b is %d\n", a, b);
+
+   swapMe(&a, &b);
+
+   printf("a is %d b is %d\n", a, b);
+
+
+}
